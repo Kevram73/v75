3c04ed94dc5b8ca9d1e805b293caf8c5924fe800
Ephrem KODAH <ephremkodah7@gmail.com>
2024-08-12 11:32:26 +0000

    okk\

--- resources/views/admin/stats.blade.php
+++ resources/views/admin/stats.blade.php
@@ -258,7 +258,8 @@
 							</div>
 							<div class="flexbox align-items-center">
 								<h3>$100</h3>
-								<h3>$ | <small class="text-fade"> Tout inclus</small></h3>
+								<h3>$ | <small class="text-fade"> Tout
+									 inclus</small></h3>
 							</div>
 							{{-- <div class="mt-10 mt-md-0">
 								<a href="#" class="waves-effect waves-light btn btn-outline btn-success"><i class="fa fa-calendar" aria-hidden="true"></i> Start SIP</a>
