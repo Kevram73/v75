0866c3485dd90724c6955e96f66827671c2d4edb
kevin <87210876+Kevram73@users.noreply.github.com>
2024-11-24 19:50:29 +0000

    cool

--- app/Http/Controllers/Admin/TransactionController.php
+++ app/Http/Controllers/Admin/TransactionController.php
@@ -124,8 +124,7 @@ class TransactionController extends Controller
 
     public function list_deposits(Request $request)
     {
-        $deposits = Transaction::where('sender_id', Auth::guard('client')->user()->id)
-            ->where('type', 'deposit')
+        $deposits = Transaction::where('type', 'deposit')
             ->orderBy('date_sent', 'desc')
             ->get();
 
@@ -134,8 +133,7 @@ class TransactionController extends Controller
 
     public function list_withdrawals(Request $request)
     {
-        $withdrawals = Transaction::where('receiver_id', Auth::guard('client')->user()->id)
-            ->where('type', 'withdrawal')
+        $withdrawals = Transaction::where('type', 'withdrawal')
             ->orderBy('date_sent', 'desc')
             ->get();
 
@@ -147,9 +145,7 @@ class TransactionController extends Controller
      */
     public function get_all_transactions(Request $request)
     {
-        $transactions = Transaction::with(['sender', 'receiver'])
-            ->orderBy('date_sent', 'desc')
-            ->get();
+        $transactions = Transaction::all();
 
         return view('admin.transactions.index', compact('transactions'));
     }
